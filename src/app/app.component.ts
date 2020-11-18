6741d330a6d7496dbbf2d225159b6b7ffe9a1a8e
Juan <breicom@gmail.com>
2020-11-18 14:24:52 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'skadi';
+export class AppComponent implements OnInit{
+  skadi = new Date('2020-11-16 18:10:00:00');
+  skadiNextSpawn: Date = new Date();
+  bron = new Date('2020-11-16 18:50:00:00');
+  bronNextSpawn: Date = new Date();
+  timeTo: any;
+  // offset = new Date().getTimezoneOffset();
+  diasLst: any[] = [];
+  diasBronLst: any[] = [];
+
+  constructor() {}
+
+  addMinutes(date: Date, m: number): Date {
+    const newDateObj = new Date(date.getTime() + m * 60000);
+    return newDateObj;
+  }
+
+  nextSpawn(who: string): any {
+    let newSpawn;
+    if (who === 'skadi') {
+      for (let i = 0; i < this.diasLst.length; i++) {
+        if (+this.diasLst[i] - +new Date() > 0) {
+          newSpawn = this.diasLst[i];
+          i = this.diasLst.length;
+        }
+      }
+    } else if (who === 'bron') {
+      for (let i = 0; i < this.diasBronLst.length; i++) {
+        if (+this.diasBronLst[i] - +new Date() > 0) {
+          newSpawn = this.diasBronLst[i];
+          i = this.diasBronLst.length;
+        }
+      }
+    }
+    return newSpawn;
+  }
+
+  crearDias(fecha: Date): any[] {
+    const dias = [];
+    for (let i = 0; i <= 42; i++) {
+      fecha = this.addMinutes(fecha, 200);
+      dias.push(fecha);
+    }
+    return dias;
+  }
+
+  ngOnInit(): any {
+    this.diasLst = this.crearDias(this.skadi);
+    this.diasBronLst = this.crearDias(this.bron);
+    this.skadiNextSpawn = this.nextSpawn('skadi');
+    this.bronNextSpawn = this.nextSpawn('bron');
+    setInterval(() => {
+      const secondsSkadi = Math.abs(-this.skadiNextSpawn - -new Date()) / 1000;
+      const secondsBron = Math.abs(-this.bronNextSpawn - -new Date()) / 1000;
+      this.timeTo = {
+        skadi: secondsSkadi,
+        bron: secondsBron
+      };
+    }, 1000);
+  }
+
 }
